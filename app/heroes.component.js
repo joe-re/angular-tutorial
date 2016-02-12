66d14d9875c4b2bc2177a8cb6d16ba8821f4af9f
joe-re <joe.tialtngo@gmail.com>
2016-02-13 06:20:09 +0900

    Select a Hero in the HeroesComponent

--- app/heroes.component.js
+++ app/heroes.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero-detail.component', './hero.service'], function(exports_1) {
+System.register(['angular2/core', './hero-detail.component', './hero.service', 'angular2/router'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_detail_component_1, hero_service_1;
+    var core_1, hero_detail_component_1, hero_service_1, router_1;
     var HeroesComponent;
     return {
         setters:[
@@ -20,10 +20,14 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
             },
             function (hero_service_1_1) {
                 hero_service_1 = hero_service_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             HeroesComponent = (function () {
-                function HeroesComponent(_heroService) {
+                function HeroesComponent(_router, _heroService) {
+                    this._router = _router;
                     this._heroService = _heroService;
                     this.title = 'Tour of Heroes';
                 }
@@ -38,14 +42,17 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
                 HeroesComponent.prototype.onSelect = function (hero) {
                     this.selectedHero = hero;
                 };
+                HeroesComponent.prototype.gotoDetail = function () {
+                    this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+                };
                 HeroesComponent = __decorate([
                     core_1.Component({
                         selector: 'my-heroes',
-                        template: "\n      <h2>My Heroes</h2>\n      <ul class=\"heroes\">\n        <li *ngFor=\"#hero of heroes\"\n         (click)=\"onSelect(hero)\"\n         [class.selected]=\"hero === selectedHero\">\n          <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n        </li>\n      </ul>\n      <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n      ",
-                        styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 10em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0em;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .heroes li.selected:hover {\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #EEE;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0em 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px ;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0px 0px 4px;\n    }\n  "],
+                        templateUrl: 'app/heroes.component.html',
+                        styleUrls: ['app/heroes.component.css'],
                         directives: [hero_detail_component_1.HeroDetailComponent]
                     }), 
-                    __metadata('design:paramtypes', [hero_service_1.HeroService])
+                    __metadata('design:paramtypes', [router_1.Router, hero_service_1.HeroService])
                 ], HeroesComponent);
                 return HeroesComponent;
             })();
