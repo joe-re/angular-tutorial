287d1129134a51e3e50f8f444cd316a3259af409
joe-re <joe.tialtngo@gmail.com>
2016-02-13 05:47:44 +0900

    Revise the HeroDetailComponent

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,19 +1,24 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit } from 'angular2/core';
 import {Hero} from './hero';
+import {RouteParams} from 'angular2/router';
+import {HeroService} from './hero.service';
+
 @Component({
   selector: 'my-hero-detail',
-  template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-  `,
+  templateUrl: 'app/hero-detail.component.html',
   inputs: ['hero']
 })
 export class HeroDetailComponent {
   hero: Hero;
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams) {
+  }
+  ngOnInit() {
+    let id = +this._routeParams.get('id');
+    this._heroService.getHero(id).then(hero => this.hero = hero);
+  }
+  goBack() {
+    window.history.back();
+  }
 }
